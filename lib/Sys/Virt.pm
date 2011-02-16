937a2cd2c8c2b0d6692de05d25d1639a5e753b00
Daniel P. Berrange <dan@berrange.com>
2011-02-16 20:54:47 +0000

    Update for 0.2.6 release

--- lib/Sys/Virt.pm
+++ lib/Sys/Virt.pm
@@ -70,7 +70,7 @@ use Sys::Virt::Secret;
 use Sys::Virt::NWFilter;
 use Sys::Virt::DomainSnapshot;
 
-our $VERSION = '0.2.5';
+our $VERSION = '0.2.6';
 require XSLoader;
 XSLoader::load('Sys::Virt', $VERSION);
 
