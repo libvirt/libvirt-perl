cfc75e62e8ef9c591f477b796cca352eee8a98c0
Daniel P. Berrange <berrange@redhat.com>
2011-06-29 11:45:35 +0100

    Update for 0.2.7 release

--- lib/Sys/Virt.pm
+++ lib/Sys/Virt.pm
@@ -71,7 +71,7 @@ use Sys::Virt::NWFilter;
 use Sys::Virt::DomainSnapshot;
 use Sys::Virt::Stream;
 
-our $VERSION = '0.2.6';
+our $VERSION = '0.2.7';
 require XSLoader;
 XSLoader::load('Sys::Virt', $VERSION);
 
