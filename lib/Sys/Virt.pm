26a23d60da2c5f220987438136e6a0517540b669
Daniel P. Berrange <berrange@redhat.com>
2012-02-13 17:51:12 +0000

    Update for 0.9.10 release

--- lib/Sys/Virt.pm
+++ lib/Sys/Virt.pm
@@ -71,7 +71,7 @@ use Sys::Virt::NWFilter;
 use Sys::Virt::DomainSnapshot;
 use Sys::Virt::Stream;
 
-our $VERSION = '0.9.9';
+our $VERSION = '0.9.10';
 require XSLoader;
 XSLoader::load('Sys::Virt', $VERSION);
 
