43d99aa338128e6c6fb16afa2419459d27152896
Daniel P. Berrange <berrange@redhat.com>
2012-09-26 12:29:34 +0100

    Update for 0.10.2 release
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>

--- lib/Sys/Virt.pm
+++ lib/Sys/Virt.pm
@@ -78,7 +78,7 @@ use Sys::Virt::NWFilter;
 use Sys::Virt::DomainSnapshot;
 use Sys::Virt::Stream;
 
-our $VERSION = '0.10.0';
+our $VERSION = '0.10.2';
 require XSLoader;
 XSLoader::load('Sys::Virt', $VERSION);
 
