c11ac02718828888d5168458dcce8acb2bd5fcd8
Daniel P. Berrange <berrange@redhat.com>
2015-01-27 13:38:46 +0000

    Bump version to 1.2.13 for new dev cycle

--- lib/Sys/Virt.pm
+++ lib/Sys/Virt.pm
@@ -78,7 +78,7 @@ use Sys::Virt::NWFilter;
 use Sys::Virt::DomainSnapshot;
 use Sys::Virt::Stream;
 
-our $VERSION = '1.2.12';
+our $VERSION = '1.2.13';
 require XSLoader;
 XSLoader::load('Sys::Virt', $VERSION);
 
