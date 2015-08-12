b72cc006ff2e28396fa2bef5e83c2f0f963c907d
Daniel P. Berrange <berrange@redhat.com>
2015-08-12 15:14:52 +0100

    Post release version bump to 1.2.19

--- lib/Sys/Virt.pm
+++ lib/Sys/Virt.pm
@@ -78,7 +78,7 @@ use Sys::Virt::NWFilter;
 use Sys::Virt::DomainSnapshot;
 use Sys::Virt::Stream;
 
-our $VERSION = '1.2.18';
+our $VERSION = '1.2.19';
 require XSLoader;
 XSLoader::load('Sys::Virt', $VERSION);
 
