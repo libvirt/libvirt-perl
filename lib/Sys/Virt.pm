1aa979ecfa1eab41793d0baa5f8ce4cea98f3ca1
Daniel P. Berrange <berrange@redhat.com>
2015-09-03 10:13:11 +0100

    Post release version bmp to 1.2.20

--- lib/Sys/Virt.pm
+++ lib/Sys/Virt.pm
@@ -78,7 +78,7 @@ use Sys::Virt::NWFilter;
 use Sys::Virt::DomainSnapshot;
 use Sys::Virt::Stream;
 
-our $VERSION = '1.2.19';
+our $VERSION = '1.2.20';
 require XSLoader;
 XSLoader::load('Sys::Virt', $VERSION);
 
