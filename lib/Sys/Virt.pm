75c7c12930bdce665664cf71f604c11a6ee37d42
Pavel Hrdina <phrdina@redhat.com>
2015-11-24 13:39:31 +0100

    Post-release version bump to 1.3.0
    Signed-off-by: Pavel Hrdina <phrdina@redhat.com>

--- lib/Sys/Virt.pm
+++ lib/Sys/Virt.pm
@@ -78,7 +78,7 @@ use Sys::Virt::NWFilter;
 use Sys::Virt::DomainSnapshot;
 use Sys::Virt::Stream;
 
-our $VERSION = '1.2.21';
+our $VERSION = '1.3.0';
 require XSLoader;
 XSLoader::load('Sys::Virt', $VERSION);
 
