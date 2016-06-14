018d8f65d4c976e329afc3d4400e638a935143c6
Daniel P. Berrange <berrange@redhat.com>
2016-06-14 15:25:29 +0100

    Change next version to 2.0.0

--- lib/Sys/Virt.pm
+++ lib/Sys/Virt.pm
@@ -78,7 +78,7 @@ use Sys::Virt::NWFilter;
 use Sys::Virt::DomainSnapshot;
 use Sys::Virt::Stream;
 
-our $VERSION = '1.3.6';
+our $VERSION = '2.0.0';
 require XSLoader;
 XSLoader::load('Sys::Virt', $VERSION);
 
