fa9c84bdb77811e2adc6f896b62fdefd9bbfedfa
Daniel P. Berrange <berrange@redhat.com>
2016-07-01 15:45:41 +0100

    Post-release version bump to 2.1.0

--- lib/Sys/Virt.pm
+++ lib/Sys/Virt.pm
@@ -78,7 +78,7 @@ use Sys::Virt::NWFilter;
 use Sys::Virt::DomainSnapshot;
 use Sys::Virt::Stream;
 
-our $VERSION = '2.0.0';
+our $VERSION = '2.1.0';
 require XSLoader;
 XSLoader::load('Sys::Virt', $VERSION);
 
