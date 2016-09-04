b8fd516f6de65e7c8e83e85faee7583a63e446da
Michal Privoznik <mprivozn@redhat.com>
2016-09-04 12:46:59 +0200

    Bump version to 2.3.0
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- lib/Sys/Virt.pm
+++ lib/Sys/Virt.pm
@@ -78,7 +78,7 @@ use Sys::Virt::NWFilter;
 use Sys::Virt::DomainSnapshot;
 use Sys::Virt::Stream;
 
-our $VERSION = '2.2.0';
+our $VERSION = '2.3.0';
 require XSLoader;
 XSLoader::load('Sys::Virt', $VERSION);
 
