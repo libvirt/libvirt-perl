461c0fa163f6978f77d8a52c0131e6fe62ff8933
Daniel P. Berrange <berrange@redhat.com>
2017-03-07 09:57:41 +0000

    Post-releae version bump to 3.2.0
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>

--- lib/Sys/Virt.pm
+++ lib/Sys/Virt.pm
@@ -78,7 +78,7 @@ use Sys::Virt::NWFilter;
 use Sys::Virt::DomainSnapshot;
 use Sys::Virt::Stream;
 
-our $VERSION = '3.1.0';
+our $VERSION = '3.2.0';
 require XSLoader;
 XSLoader::load('Sys::Virt', $VERSION);
 
