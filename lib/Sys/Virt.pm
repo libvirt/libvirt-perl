4ab9641f216c220c9839268fa1d13876c806b80a
Daniel P. Berrange <berrange@redhat.com>
2017-04-03 14:51:14 +0100

    Update version numbers to 3.3.0
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>

--- lib/Sys/Virt.pm
+++ lib/Sys/Virt.pm
@@ -78,7 +78,7 @@ use Sys::Virt::NWFilter;
 use Sys::Virt::DomainSnapshot;
 use Sys::Virt::Stream;
 
-our $VERSION = '3.2.0';
+our $VERSION = '3.3.0';
 require XSLoader;
 XSLoader::load('Sys::Virt', $VERSION);
 
