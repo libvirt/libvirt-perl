e80631c910f3bc2028048638fefbe62fdacc6a91
Daniel P. Berrange <berrange@redhat.com>
2017-12-05 11:46:10 +0000

    Post-release version bump to 4.0.0
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>

--- lib/Sys/Virt.pm
+++ lib/Sys/Virt.pm
@@ -78,7 +78,7 @@ use Sys::Virt::NWFilter;
 use Sys::Virt::DomainSnapshot;
 use Sys::Virt::Stream;
 
-our $VERSION = '3.9.1';
+our $VERSION = '4.0.0';
 require XSLoader;
 XSLoader::load('Sys::Virt', $VERSION);
 
