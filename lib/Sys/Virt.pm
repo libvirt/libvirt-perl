962c1c059ea9f1664189b8fd5cc48d1286ddd52b
Daniel P. Berrange <berrange@redhat.com>
2017-12-05 11:42:23 +0000

    Set release to 3.9.1 for bugfixes only
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>

--- lib/Sys/Virt.pm
+++ lib/Sys/Virt.pm
@@ -78,7 +78,7 @@ use Sys::Virt::NWFilter;
 use Sys::Virt::DomainSnapshot;
 use Sys::Virt::Stream;
 
-our $VERSION = '3.9.0';
+our $VERSION = '3.9.1';
 require XSLoader;
 XSLoader::load('Sys::Virt', $VERSION);
 
