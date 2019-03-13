4384d0021e9a4c1f463bed8ab594d8ae658236eb
Daniel P. Berrangé <berrange@redhat.com>
2019-03-13 14:12:20 +0000

    Post release version bump to 5.2.0
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

--- lib/Sys/Virt.pm
+++ lib/Sys/Virt.pm
@@ -79,7 +79,7 @@ use Sys::Virt::NWFilterBinding;
 use Sys::Virt::DomainSnapshot;
 use Sys::Virt::Stream;
 
-our $VERSION = '5.1.0';
+our $VERSION = '5.2.0';
 require XSLoader;
 XSLoader::load('Sys::Virt', $VERSION);
 
