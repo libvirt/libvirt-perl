248c9a3a7ea83d86aef99ea7833c76856766b4be
Daniel P. Berrangé <berrange@redhat.com>
2020-01-16 16:52:25 +0000

    Version bump to 6.1.0
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

--- lib/Sys/Virt.pm
+++ lib/Sys/Virt.pm
@@ -81,7 +81,7 @@ use Sys::Virt::DomainSnapshot;
 use Sys::Virt::DomainCheckpoint;
 use Sys::Virt::Stream;
 
-our $VERSION = '6.0.0';
+our $VERSION = '6.1.0';
 require XSLoader;
 XSLoader::load('Sys::Virt', $VERSION);
 
