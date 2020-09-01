b0921eac6de61794f54b5f34b1d6d8dfb673726a
Daniel P. Berrangé <berrange@redhat.com>
2020-09-01 11:41:08 +0100

    Bump version to 6.8.0
    There were no new APIs in 6.7.0, so no release was needed.
    
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

--- lib/Sys/Virt.pm
+++ lib/Sys/Virt.pm
@@ -81,7 +81,7 @@ use Sys::Virt::DomainSnapshot;
 use Sys::Virt::DomainCheckpoint;
 use Sys::Virt::Stream;
 
-our $VERSION = '6.7.0';
+our $VERSION = '6.8.0';
 require XSLoader;
 XSLoader::load('Sys::Virt', $VERSION);
 
