libvirt/libvirt-perl

commit 1bdf37223b4870581deb0d5310489223ca34e88a

lib: Add support for virDomainSaveParams/virDomainRestoreParams
This one is a bit tricky because we've already condensed
virDomain{Save,Restore} and virDomain{Save,Restore}Flags to a single
Perl function which we now have to enhance by a third -
virDomainSaveParams/virDomainRestoreParams -variant which essentially
deprecates the former two. The $to/$from argument is now optional along
with $dxml because they're both mutually exclusive with the $params_sv
hash parameter which can contain both of the two parameters.

Signed-off-by: Erik Skultety <eskultet@redhat.com>

lib/Sys/Virt.xs
@@ -2377,25 +2377,79 @@ PREINIT:
 
 
 void
-restore_domain(con, from, dxmlsv=&PL_sv_undef, flags=0)
+restore_domain(con, fromsv=&PL_sv_undef, dxmlsv=&PL_sv_undef, params_sv=&PL_sv_undef, flags=0)
       virConnectPtr con;
-      const char *from;
+      SV *fromsv;
       SV *dxmlsv;
+      SV *params_sv;
       unsigned int flags;
  PREINIT:
+      const char *from = NULL;
       const char *dxml = NULL;
+      HV *params_hv = NULL;
+      virTypedParameterPtr params;
+      int nparams;
   PPCODE:
+      if (!from && !SvOK(params_sv))
+          croak("Either $from or $params parameter must be supplied");
+
       if (SvOK(dxmlsv))
 	  dxml = SvPV_nolen(dxmlsv);
 
-      if (dxml || flags) {
-          if (virDomainRestoreFlags(con, from, dxml, flags) < 0)
-              _croak_error();
-      } else {
-          if (virDomainRestore(con, from) < 0)
-              _croak_error();
+      if (SvOK(fromsv))
+          from = SvPV_nolen(fromsv);
+
+      if (SvOK(params_sv)) {
+          params_hv = (HV*)SvRV(params_sv);
       }
 
+      if (params_hv) {
+          nparams = 2;
+
+          /* If parameters were supplied using the old way, we'll convert them
+           * to the new style libvirt API, i.e.
+           * dom->restore(filename, dxml, {FOO => bar}, flags) becomes
+           * dom->restore({SAVE_PARAM_FILE => file,
+           *               SAVE_PARAM_DXML => dxml,
+           *               FOO => bar}, flags)
+           */
+          if (from)
+              hv_store(params_hv,
+                       VIR_DOMAIN_SAVE_PARAM_FILE,
+                       strlen(VIR_DOMAIN_SAVE_PARAM_FILE),
+                       fromsv, 0);
+          if (dxml)
+              hv_store(params_hv,
+                       VIR_DOMAIN_SAVE_PARAM_DXML,
+                       strlen(VIR_DOMAIN_SAVE_PARAM_DXML),
+                       dxmlsv, 0);
+
+          Newx(params, nparams, virTypedParameter);
+          strncpy(params[0].field, VIR_DOMAIN_SAVE_PARAM_FILE,
+                  VIR_TYPED_PARAM_FIELD_LENGTH);
+          params[0].type = VIR_TYPED_PARAM_STRING;
+
+          strncpy(params[1].field, VIR_DOMAIN_SAVE_PARAM_DXML,
+                  VIR_TYPED_PARAM_FIELD_LENGTH);
+          params[1].type = VIR_TYPED_PARAM_STRING;
+
+          nparams = vir_typed_param_from_hv(params_hv, params, nparams);
+
+          if ((virDomainRestoreParams(con, params, nparams, flags)) < 0) {
+              vir_typed_param_safe_free(params, nparams);
+              _croak_error();
+          }
+          vir_typed_param_safe_free(params, nparams);
+      }
+      else {
+          if (dxml || flags) {
+              if (virDomainRestoreFlags(con, from, dxml, flags) < 0)
+                  _croak_error();
+          } else {
+              if (virDomainRestore(con, from) < 0)
+                  _croak_error();
+          }
+      }
 
 SV *
 get_save_image_xml_description(con, file, flags=0)
@@ -4485,23 +4539,79 @@ pm_wakeup(dom, flags=0)
 
 
 void
-save(dom, to, dxmlsv=&PL_sv_undef, flags=0)
+save(dom, tosv=&PL_sv_undef, dxmlsv=&PL_sv_undef, params_sv=&PL_sv_undef, flags=0)
+
       virDomainPtr dom;
-      const char *to;
+      SV *tosv;
       SV *dxmlsv;
+      SV *params_sv;
       unsigned int flags;
 PREINIT:
+      const char *to = NULL;
       const char *dxml = NULL;
+      HV *params_hv = NULL;
+      virTypedParameterPtr params;
+      int nparams;
   PPCODE:
+      if (!to && !SvOK(params_sv))
+          croak("Either $to or $params parameter must be supplied");
+
       if (SvOK(dxmlsv))
 	  dxml = SvPV_nolen(dxmlsv);
 
-      if (dxml || flags) {
-          if ((virDomainSaveFlags(dom, to, dxml, flags)) < 0)
+      if (SvOK(tosv))
+          to = SvPV_nolen(tosv);
+
+      if (SvOK(params_sv)) {
+          params_hv = (HV*)SvRV(params_sv);
+      }
+
+      if (params_hv) {
+          nparams = 2;
+
+          /* If parameters were supplied using the old way, we'll convert them
+           * to the new style libvirt API, i.e.
+           * dom->save(filename, dxml, {FOO => bar}, flags) becomes
+           * dom->save({SAVE_PARAM_FILE => file,
+           *            SAVE_PARAM_DXML => dxml,
+           *            FOO => bar}, flags)
+           */
+          if (to)
+              hv_store(params_hv,
+                       VIR_DOMAIN_SAVE_PARAM_FILE,
+                       strlen(VIR_DOMAIN_SAVE_PARAM_FILE),
+                       tosv, 0);
+          if (dxml)
+              hv_store(params_hv,
+                       VIR_DOMAIN_SAVE_PARAM_DXML,
+                       strlen(VIR_DOMAIN_SAVE_PARAM_DXML),
+                       dxmlsv, 0);
+
+          Newx(params, nparams, virTypedParameter);
+          strncpy(params[0].field, VIR_DOMAIN_SAVE_PARAM_FILE,
+                  VIR_TYPED_PARAM_FIELD_LENGTH);
+          params[0].type = VIR_TYPED_PARAM_STRING;
+
+          strncpy(params[1].field, VIR_DOMAIN_SAVE_PARAM_DXML,
+                  VIR_TYPED_PARAM_FIELD_LENGTH);
+          params[1].type = VIR_TYPED_PARAM_STRING;
+
+          nparams = vir_typed_param_from_hv(params_hv, params, nparams);
+
+          if ((virDomainSaveParams(dom, params, nparams, flags)) < 0) {
+              vir_typed_param_safe_free(params, nparams);
               _croak_error();
-      } else {
-          if ((virDomainSave(dom, to)) < 0)
-              _croak_error();
+          }
+          vir_typed_param_safe_free(params, nparams);
+      }
+      else {
+          if (dxml || flags) {
+              if ((virDomainSaveFlags(dom, to, dxml, flags)) < 0)
+                  _croak_error();
+          } else {
+              if ((virDomainSave(dom, to)) < 0)
+                  _croak_error();
+          }
       }
 
 
@@ -10342,6 +10452,8 @@ BOOT:
       REGISTER_CONSTANT(VIR_DOMAIN_DIRTYRATE_MODE_PAGE_SAMPLING, DIRTYRATE_MODE_PAGE_SAMPLING);
       REGISTER_CONSTANT(VIR_DOMAIN_DIRTYRATE_MODE_DIRTY_BITMAP, DIRTYRATE_MODE_DIRTY_BITMAP);
       REGISTER_CONSTANT(VIR_DOMAIN_DIRTYRATE_MODE_DIRTY_RING, DIRTYRATE_MODE_DIRTY_RING);
+      REGISTER_CONSTANT_STR(VIR_DOMAIN_SAVE_PARAM_FILE, SAVE_PARAM_FILE);
+      REGISTER_CONSTANT_STR(VIR_DOMAIN_SAVE_PARAM_DXML, SAVE_PARAM_DXML);
 
       stash = gv_stashpv( "Sys::Virt::DomainSnapshot", TRUE );
       REGISTER_CONSTANT(VIR_DOMAIN_SNAPSHOT_DELETE_CHILDREN, DELETE_CHILDREN);

lib/Sys/Virt.pm
@@ -1320,12 +1320,13 @@ network interfaces
 Abort a transaction for changing the configuration of one or more
 network interfaces
 
-=item $conn->restore_domain($savefile, $dxml=undef, $flags=0)
+=item $conn->restore_domain($savefile, $dxml=undef, $params=undef, $flags=0)
 
-Recreate a domain from the saved state file given in the C<$savefile> parameter.
-The optional C<$dxml> parameter can be used to alter portions of the domain XML.
-The C<$flags> parameter accepts one of the SAVE / RESTORE FLAG CONSTANTS
-described for the domain object and defaults to zero.
+Recreate a domain from the saved state file given in the C<$savefile>
+parameter. The optional C<$dxml> parameter can be used to alter portions of the
+domain XML. The C<$params> parameter is a hash reference whose keys is a subset
+of the SAVE / RESTORE PARAMETER CONSTANTS. The C<$flags> parameter accepts one
+of the SAVE / RESTORE FLAG CONSTANTS described later and defaults to zero.
 
 =item $conn->get_max_vcpus($domtype)
 

lib/Sys/Virt/Domain.pm
@@ -207,15 +207,16 @@ C<$duration> specifies when the guest should automatically
 wakeup. The C<$flags> parameter is optional and defaults to
 zero.
 
-=item $dom->save($filename, $dxml=undef, $flags=0)
+=item $dom->save($filename, $dxml=undef, $params=undef, $flags=0)
 
 Take a snapshot of the domain's state and save the information to
 the file named in the C<$filename> parameter. The domain can later
 be restored from this file with the C<restore_domain> method on
 the L<Sys::Virt> object. The optional C<$dxml> parameter can be used to alter
-portions of the domain XML that will be used when performing restore.
-The C<$flags> parameter accepts one of the SAVE / RESTORE FLAG CONSTANTS
-described later and defaults to zero.
+portions of the domain XML that will be used when performing restore.  The
+C<$params> parameter is a hash reference whose keys is a subset of the SAVE /
+RESTORE PARAMETER CONSTANTS. The C<$flags> parameter accepts one of the SAVE /
+RESTORE FLAG CONSTANTS described later and defaults to zero.
 
 =item $dom->managed_save($flags=0)
 
@@ -4139,6 +4140,21 @@ to start upon restore.
 =item Sys::Virt::Domain::SAVE_RESET_NVRAM
 
 Reset the firmware NVRAM state from its original template
+
+=back
+
+=head2 SAVE / RESTORE PARAMETER CONSTANTS
+
+=over 4
+
+=item Sys::Virt::Domain::SAVE_PARAM_FILE
+
+Specify the save state file to save to or restore from.
+
+=item Sys::Virt::Domain::SAVE_PARAM_DXML
+
+Used to adjust guest xml on restore, e.g. to alter a device while domain is
+stopped.
 
 =back
 

commit be960176a8b045d54ac1dfa0cb76cef8ed9cc660

lib: Fix parameter detection for save/restore_domain
Libvirt gained params alternative for
virDomainSave/virDomainRestore APIs. As usual in our bindings, we
don't expose these variants under new methods, rather wire them
under existing ones and then have a small code that decides
whether the old API or params API should be called. In case of
save/restore_domain this is done by checking whether path to
save/restore from was provided. However, the actual check is not
written properly and checks for decoded string rather than perl
argument. Not to mention this is done before the perl argument is
decoded.

Fixes: 1bdf37223b4870581deb0d5310489223ca34e88a
Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

lib/Sys/Virt.xs
@@ -2390,7 +2390,7 @@ restore_domain(con, fromsv=&PL_sv_undef, dxmlsv=&PL_sv_undef, params_sv=&PL_sv_u
       virTypedParameterPtr params;
       int nparams;
   PPCODE:
-      if (!from && !SvOK(params_sv))
+      if (!SvOK(fromsv) && !SvOK(params_sv))
           croak("Either $from or $params parameter must be supplied");
 
       if (SvOK(dxmlsv))
@@ -4553,7 +4553,7 @@ PREINIT:
       virTypedParameterPtr params;
       int nparams;
   PPCODE:
-      if (!to && !SvOK(params_sv))
+      if (!SvOK(tosv) && !SvOK(params_sv))
           croak("Either $to or $params parameter must be supplied");
 
       if (SvOK(dxmlsv))