93e0716e99982439c557ce88b6be51628955da90
Daniel P. Berrangé <berrange@redhat.com>
2022-07-01 11:06:14 +0100

    Post release version bump to 8.6.0
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

--- lib/Sys/Virt.pm
+++ lib/Sys/Virt.pm
@@ -81,7 +81,7 @@ use Sys::Virt::DomainSnapshot;
 use Sys::Virt::DomainCheckpoint;
 use Sys::Virt::Stream;
 
-our $VERSION = '8.5.0';
+our $VERSION = '8.6.0';
 require XSLoader;
 XSLoader::load('Sys::Virt', $VERSION);
 
