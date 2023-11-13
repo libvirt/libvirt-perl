228db7a0f540419368342a4cec233851549bb10b
Daniel P. Berrangé <berrange@redhat.com>
2023-11-13 16:57:10 +0000

    Bump version for next release cycle
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

--- lib/Sys/Virt.pm
+++ lib/Sys/Virt.pm
@@ -92,7 +92,7 @@ use Sys::Virt::DomainSnapshot;
 use Sys::Virt::DomainCheckpoint;
 use Sys::Virt::Stream;
 
-our $VERSION = '9.8.0';
+our $VERSION = '9.10.0';
 require XSLoader;
 XSLoader::load('Sys::Virt', $VERSION);
 
