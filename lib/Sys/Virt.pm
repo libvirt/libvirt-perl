bb5daf19e9d264ff8b9dd374602c4955d1418f38
Daniel P. Berrangé <berrange@redhat.com>
2024-01-17 12:52:21 +0000

    Set date for 10.0.0 release
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

--- lib/Sys/Virt.pm
+++ lib/Sys/Virt.pm
@@ -92,7 +92,7 @@ use Sys::Virt::DomainSnapshot;
 use Sys::Virt::DomainCheckpoint;
 use Sys::Virt::Stream;
 
-our $VERSION = '9.10.0';
+our $VERSION = '10.0.0';
 require XSLoader;
 XSLoader::load('Sys::Virt', $VERSION);
 
