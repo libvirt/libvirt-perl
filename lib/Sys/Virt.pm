3ba6f2a51cde16f147b8c6c53b36075d3f72271a
Daniel P. Berrangé <berrange@redhat.com>
2024-03-01 17:29:49 +0000

    Bump for 10.2.0 release cycle
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

--- lib/Sys/Virt.pm
+++ lib/Sys/Virt.pm
@@ -92,7 +92,7 @@ use Sys::Virt::DomainSnapshot;
 use Sys::Virt::DomainCheckpoint;
 use Sys::Virt::Stream;
 
-our $VERSION = '10.1.0';
+our $VERSION = '10.2.0';
 require XSLoader;
 XSLoader::load('Sys::Virt', $VERSION);
 
