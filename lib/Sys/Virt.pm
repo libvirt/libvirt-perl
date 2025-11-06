b179b2a0e9bc1c63c1e3f7091f950f727ab6a4b0
Daniel P. Berrangé <berrange@redhat.com>
2025-11-06 17:16:55 +0000

    Revert "Bump version to 11.10.0"
    This reverts commit 7466a6e28ae04ebc1de8dbe47f335747cab10a22.
    
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

--- lib/Sys/Virt.pm
+++ lib/Sys/Virt.pm
@@ -92,7 +92,7 @@ use Sys::Virt::DomainSnapshot;
 use Sys::Virt::DomainCheckpoint;
 use Sys::Virt::Stream;
 
-our $VERSION = '11.10.0';
+our $VERSION = '11.8.0';
 require XSLoader;
 XSLoader::load('Sys::Virt', $VERSION);
 
