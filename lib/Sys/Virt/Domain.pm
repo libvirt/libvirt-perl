libvirt/libvirt-perl

commit 44fb0cd06989bbf372aa6f012be3160fc9bb29d7

Add support for VIR_DOMAIN_EVENT_ID_MIGRATION_ITERATION callback
Signed-off-by: Daniel P. Berrange <berrange@redhat.com>

lib/Sys/Virt/Domain.pm
@@ -2899,6 +2899,12 @@ The agent is now disconnected
 The second parameter, C<reason>, matches one of the following
 constants
 
+=item Sys::Virt::Domain::EVENT_ID_MIGRATION_ITERATION
+
+Domain migration progress iteration. The C<iteration> parameter
+to the callback will specify the number of iterations migration
+has made over guest RAM.
+
 =over 4
 
 =item Sys::Virt::Domain::EVENT_AGENT_LIFECYCLE_REASON_UNKNOWN

Virt.xs
@@ -1053,6 +1053,44 @@ _domain_event_agent_lifecycle_callback(virConnectPtr con,
 
 
 static int
+_domain_event_migration_iteration_callback(virConnectPtr con,
+					   virDomainPtr dom,
+					   int iteration,
+					   void *opaque)
+{
+    AV *data = opaque;
+    SV **self;
+    SV **cb;
+    SV *domref;
+    dSP;
+
+    self = av_fetch(data, 0, 0);
+    cb = av_fetch(data, 1, 0);
+
+    SvREFCNT_inc(*self);
+
+    ENTER;
+    SAVETMPS;
+
+    PUSHMARK(SP);
+    XPUSHs(*self);
+    domref = sv_newmortal();
+    sv_setref_pv(domref, "Sys::Virt::Domain", (void*)dom);
+    virDomainRef(dom);
+    XPUSHs(domref);
+    XPUSHs(sv_2mortal(newSViv(iteration)));
+    PUTBACK;
+
+    call_sv(*cb, G_DISCARD);
+
+    FREETMPS;
+    LEAVE;
+
+    return 0;
+}
+
+
+static int
 _network_event_lifecycle_callback(virConnectPtr con,
 				  virNetworkPtr net,
 				  int event,
@@ -3027,6 +3065,9 @@ PREINIT:
           break;
       case VIR_DOMAIN_EVENT_ID_AGENT_LIFECYCLE:
           callback = VIR_DOMAIN_EVENT_CALLBACK(_domain_event_agent_lifecycle_callback);
+          break;
+      case VIR_DOMAIN_EVENT_ID_MIGRATION_ITERATION:
+          callback = VIR_DOMAIN_EVENT_CALLBACK(_domain_event_migration_iteration_callback);
           break;
       default:
           callback = VIR_DOMAIN_EVENT_CALLBACK(_domain_event_generic_callback);
@@ -7696,6 +7737,7 @@ BOOT:
       REGISTER_CONSTANT(VIR_DOMAIN_EVENT_ID_DEVICE_REMOVED, EVENT_ID_DEVICE_REMOVED);
       REGISTER_CONSTANT(VIR_DOMAIN_EVENT_ID_TUNABLE, EVENT_ID_TUNABLE);
       REGISTER_CONSTANT(VIR_DOMAIN_EVENT_ID_AGENT_LIFECYCLE, EVENT_ID_AGENT_LIFECYCLE);
+      REGISTER_CONSTANT(VIR_DOMAIN_EVENT_ID_MIGRATION_ITERATION, EVENT_ID_MIGRATION_ITERATION);
 
       REGISTER_CONSTANT(VIR_DOMAIN_EVENT_WATCHDOG_NONE, EVENT_WATCHDOG_NONE);
       REGISTER_CONSTANT(VIR_DOMAIN_EVENT_WATCHDOG_PAUSE, EVENT_WATCHDOG_PAUSE);

t/030-api-coverage.t
@@ -91,6 +91,7 @@ virConnectDomainEventDeviceAddedCallback
 virConnectDomainEventDeviceRemovedCallback
 virConnectDomainEventTunableCallback
 virConnectDomainEventAgentLifecycleCallback
+virConnectDomainEventMigrationIterationCallback
 
 virConnectNetworkEventLifecycleCallback
 

commit bf11392b202760638497afd75053691acc0868a0

Sys::Virt::Domain: Fix placement of EVENT_ID_MIGRATION_ITERATION docs
The current placement of documentation of
EVENT_ID_MIGRATION_ITERATION is a bit unfortunate as it lives
right in the middle of possible values for agent lifecycle
reasons block. Move it further a bit.

Fixes: 44fb0cd06989bbf372aa6f012be3160fc9bb29d7
Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

lib/Sys/Virt/Domain.pm
@@ -4305,12 +4305,6 @@ The agent is now disconnected
 The second parameter, C<reason>, matches one of the following
 constants
 
-=item Sys::Virt::Domain::EVENT_ID_MIGRATION_ITERATION
-
-Domain migration progress iteration. The C<iteration> parameter
-to the callback will specify the number of iterations migration
-has made over guest RAM.
-
 =over 4
 
 =item Sys::Virt::Domain::EVENT_AGENT_LIFECYCLE_REASON_UNKNOWN
@@ -4326,6 +4320,12 @@ The domain was initially booted
 The channel on a running guest changed state
 
 =back
+
+=item Sys::Virt::Domain::EVENT_ID_MIGRATION_ITERATION
+
+Domain migration progress iteration. The C<iteration> parameter
+to the callback will specify the number of iterations migration
+has made over guest RAM.
 
 =item Sys::Virt::Domain::EVENT_ID_JOB_COMPLETED
 